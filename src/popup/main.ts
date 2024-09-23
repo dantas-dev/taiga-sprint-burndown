fec46807f05a5fa3bb4383e62354e940a76f2df8
Daniel Vinícius <viniccius774@gmail.com>
2024-09-23 18:39:26 -0300

    feat: adiciona estilo do taiga aos elementos e remove conteúdo do popup

--- src/popup/main.ts
+++ src/popup/main.ts
@@ -49,53 +49,6 @@ document.addEventListener("DOMContentLoaded", () => {
         aggregatedMembersInfo,
         totalNewHR,
       }) => {
-        document.getElementById("squad-name").textContent =
-          squadName || "NOT FOUND";
-        document.getElementById("duration").textContent =
-          duration || "NOT FOUND";
-        document.getElementById("total-hr").textContent =
-          `${checkNaN(totalClosedHR)} / ${checkNaN(totalHR)} (${checkNaN(
-            remainingHours
-          )} - ${checkNaN(totalPercent)}%)` || "NOT FOUND";
-        document.getElementById("qtd-closed").textContent =
-          totalClosed || "NOT FOUND";
-        document.getElementById("qtd-new").textContent =
-          totalNew || "NOT FOUND";
-        document.getElementById("qtd-new-hr").textContent =
-          totalNewHR || "NOT FOUND";
-
-        const totalOfTotalTypes = Object.values(totalTypes).reduce(
-          (acc: number, curr: number) => acc + curr,
-          0
-        );
-        document.getElementById(
-          "qtd-total"
-        ).textContent = `${totalOfTotalTypes} (${Object.entries(totalTypes)
-          .map(([key, value]) => `${key}: ${value}`)
-          .join(", ")})`;
-
-        document.getElementById("storys").innerHTML = storys
-          .map(
-            (story) => `
-          <li>
-            <strong>${story.name}</strong>
-            <ul>
-              <li>TOTAL (HR): ${checkNaN(story.totalHR)} (${checkNaN(
-              story.remainingHours
-            )} - ${checkNaN(story.totalPercent)}%)</li>
-              <li>CLOSED (HR): ${checkNaN(
-                story.totalClosedHR
-              )} / NEW CLOSED: ${checkNaN(story.totalNewHR)}</li>
-              <li>TASKS (QTD): ${story.tasks.length} (CLOSED: ${
-              story.totalClosed
-            } / NEW: ${story.totalNew})</li>
-            </ul>
-          </li>
-        `
-          )
-          .join("");
-
-        fillMembersTable("members-info", aggregatedMembersInfo);
         activateCopyButton();
 
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
